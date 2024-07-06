0078efc6670a3831d051a6e710e08ff14a3fa93f
NaijasonEzeiru <jayseehe1035@gmail.com>
2024-07-06 13:23:10 +0100

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,xsl}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryBg: "var(--primary-bg-color)",
+        secondaryBg: "var(--secondary-bg-color)",
+        goldColor: "var(--gold-color)",
+        primaryText: "var(--primary-text-color)",
+        secondaryBg2: "var(--secondary-bg-color-2)",
+        primaryCta: "var(--primary-cta-color)",
+        // disabledColor: 'var(--disabled-gold-color)'
+      },
+      aspectRatio: {
+        "4/3": "4 / 3",
+        "3/2": "3 / 2",
+      },
+    },
+  },
+  plugins: [],
+};
